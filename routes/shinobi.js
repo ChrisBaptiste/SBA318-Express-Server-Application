cf7e0a172a4842ed3aa0fbcc84053736e3b5af07
Urick_Baptiste <chrisbaptiste19@gmail.com>
2025-04-15 01:30:09 -0400

    set up route for getting all ninjas in routes/shinobi.js

--- routes/shinobi.js
+++ routes/shinobi.js
@@ -3,3 +3,21 @@ const router = express.Router();
 
 // Creating in memory storage for ninjas array
 let ninjas = [];
+
+// Setting up route for getting all ninjas (with optional filtering by village)
+router.get("/", (req, res) => {
+    var filtered = [];
+    var village = req.query.village;
+    if (village) {
+      // Filtering ninjas by village
+      for (var i = 0; i < ninjas.length; i++) {
+        if (ninjas[i].village === village) {
+          filtered.push(ninjas[i]);
+        }
+      }
+    } else {
+      filtered = ninjas;
+    }
+    // Rendering index view with ninjas data
+    res.render("index", { ninjas: filtered });
+  });
